25232a93252f9d5446005f85b21fcf0ae29f95bd
Daniel José dos Santos <daniel.santos.ap@gmail.com>
2021-07-16 22:00:53 -0300

    começando a bombar

--- db.php
+++ db.php
@@ -9,9 +9,10 @@ namespace Kingkernel\Database;
 
 abstract class DB 
 {
-    public $connection;
-    public $config;
-    public static $teste;
+    private static $connect;
+    private static $queryString;
+    private static $query;
+    private static $data;
 
     public function __construct()
     {
@@ -22,55 +23,30 @@ abstract class DB
         $_ENV["DATABASE"] = parse_ini_file(".env");
         return $_ENV["DATABASE"];
     }
-    public static function load_databases()
+    public static function connect()
     {
         $dbInfo = self::init();
-        $dbuser = $dbInfo["DB_USER"];
         $pdoConfig  = $dbInfo["DB_DRIVER"] . ":". "host=" . $dbInfo["DB_HOST"] . ";";
         $pdoConfig .= "dbname=".$dbInfo["DB_NAME"];
         try {
             $connection = new \PDO($pdoConfig, $dbInfo["DB_USER"], $dbInfo['DB_PASSWORD']);
             $connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-        } catch (\Throwable $th) {
-            //throw $th;
-            print_r($th);
+            return $connection;
+            } catch (\Throwable $th) {
+            return $th;
         };
-        //print_r($dbInfo);
-        //print_r($pdoConfig);
-        //print_r(self::init());
-        //$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        //$sql = 'show databases';
-        //$query = $connection->query($sql);
-        //$query->fetcAll();
-        //print_r($query);
     }
-    public static function teste()
+    public static function select($query)
     {
-        print_r($this);
+        $cn = self::connect();
+        self::$queryString = $query;
+        self::$query = $cn->query(self::$queryString);
+        self::$data = self::$query->fetchall();
+        //return self::$data;
+        self::echo ();
     }
-    public static function getConnection() {
-
-        switch (DB_DRIVER){
-            case 'sqlsrv':
-                $pdoConfig  = DB_DRIVER . ":". "Server=" . DB_HOST . ";";
-                $pdoConfig .= "Database=".DB_NAME.";";
-            break;
-            case 'mysql' :
-                $pdoConfig  = DB_DRIVER . ":". "host=" . DB_HOST . ";";
-                $pdoConfig .= "dbname=".DB_NAME.";";
-            break;
-            }
-
-        try {
-            if(!isset($connection)){
-                $connection =  new PDO($pdoConfig, DB_USER, DB_PASSWORD);
-                $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            }
-                return $connection;
-            } catch (PDOException $e) {
-                $mensagem = "Drivers disponiveis: " . implode(",", PDO::getAvailableDrivers());
-                $mensagem .= "\nErro: " . $e->getMessage();
-                throw new Exception($mensagem);
-         }
-     }
+    public static function echo()
+    {
+        print_r(self::$data);
+    }
 }
--- index.php
+++ index.php
@@ -3,23 +3,4 @@ session_start();
 require(__DIR__ .'/vendor/autoload.php');
 
 use Kingkernel\Database\DB;
-//$_SESSION["DB"] = parse_ini_file(".env");
-//$_ENV["DBCONFIG"] = parse_ini_file(".env");
-//print_r($_ENV["DBCONFIG"]);
-//echo "ola";
-DB::load_databases();
-
-/*
-try{
-
-    $Conexao = Conexao::getConnection();
-    $query = $Conexao->query('show databases');
-    $tabelas = $query->fetchAll();
-
- }catch(Exception $e){
-
-    echo $e->getMessage();
-    exit;
-
- }
-*/
+DB::select("show tables");
